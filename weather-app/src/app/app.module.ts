4de0e95d4362a8ec902b423cf47a584e9d5c36ee
sai likhit <sailikhit1999@gmail.com>
2020-07-05 21:36:14 +0530

    1st commit for weather app

---
+++ weather-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import {RouterModule} from '@angular/router';
+import {allAppRoutes} from './routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(allAppRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
